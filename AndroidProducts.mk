d69193651478eb74fd5142f01059059e2d1eb76c
Martincz Gao <martincz@mokeedev.com>
2022-07-07 23:41:25 +0800

    apollon: mokee-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_apollon.mk
+    $(LOCAL_DIR)/mokee_apollon.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_apollon-user \
-    lineage_apollon-userdebug \
-    lineage_apollon-eng
+    mokee_apollon-user \
+    mokee_apollon-userdebug \
+    mokee_apollon-eng
